a15c8550fdfb312cf52feb765b5308dfaabbbe8f
Saverio Follador <saverio@moku.io>
2019-02-03 16:51:01 +0100

    Updates

---
+++ precache-manifest.6d78fb355b7df4eee74270608bbfd58e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf200c648dbcd864967",
+    "url": "/threejs-labyrinth/static/js/runtime~main.adf200c6.js"
+  },
+  {
+    "revision": "6d49eb4851e33975f74a",
+    "url": "/threejs-labyrinth/static/js/main.6d49eb48.chunk.js"
+  },
+  {
+    "revision": "c796514eb23fb05c6ccd",
+    "url": "/threejs-labyrinth/static/js/1.c796514e.chunk.js"
+  },
+  {
+    "revision": "6d49eb4851e33975f74a",
+    "url": "/threejs-labyrinth/static/css/main.bdf7412a.chunk.css"
+  },
+  {
+    "revision": "57e79791bae3b260aa45509bd4a4486e",
+    "url": "/threejs-labyrinth/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e35e57de0ae74af35dd779b731ec9a9.js"
+  "/threejs-labyrinth/precache-manifest.6d78fb355b7df4eee74270608bbfd58e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/threejs-labyrinth/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
