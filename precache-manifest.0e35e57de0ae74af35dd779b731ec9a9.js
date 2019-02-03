bd9694ac77a1910d2e695fc5d949e03ab9e81522
Saverio Follador <saverio@moku.io>
2019-02-03 16:46:42 +0100

    Updates

---
+++ precache-manifest.0e35e57de0ae74af35dd779b731ec9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a3d156dc5da5c38ff2e",
+    "url": "/static/js/main.0a3d156d.chunk.js"
+  },
+  {
+    "revision": "c796514eb23fb05c6ccd",
+    "url": "/static/js/1.c796514e.chunk.js"
+  },
+  {
+    "revision": "0a3d156dc5da5c38ff2e",
+    "url": "/static/css/main.bdf7412a.chunk.css"
+  },
+  {
+    "revision": "c2ad426ecac8a8799777c76c62807b4a",
+    "url": "/index.html"
+  }
+];
