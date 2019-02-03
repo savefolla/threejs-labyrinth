6ca77f433b82208e67bc292094cba4fc19cd2c07
Saverio Follador <saverio@moku.io>
2019-02-03 13:31:17 +0100

    setup threejs exampel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import * as THREE from 'three';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.myRef = React.createRef();
+    this.scene = undefined;
+    this.camera = undefined;
+    this.renderer = undefined;
+    this.cube = undefined;
+
+    this.animate = this.animate.bind(this);
+    this.setup = this.setup.bind(this);
+  }
+
+  componentDidMount() {
+    this.setup();
+    this.animate();
+  }
+
+  setup() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.myRef.current.appendChild(this.renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.camera.position.z = 5;
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate);
+
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div ref={this.myRef}>
       </div>
     );
   }
